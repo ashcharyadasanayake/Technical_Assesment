45084b46d99dc5f69d32e02b2275f2794fada5a2
ashcharyadasanayake <aishadasana@gmail.com>
2024-05-09 15:46:57 +0530

    bug fix in myapp urls

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -4,10 +4,10 @@ from myapp.views import table1_view, table2_view,table3_view,table4_view,table5_
 urlpatterns = [
     path('table1/', table1_view, name='table1'),
     path('table2/', table2_view, name='table2'),
-    path('table3/', table3_view, name='table2'),
-    path('table4/', table4_view, name='table2'),
-    path('table5/', table5_view, name='table2'),
-    path('table6/', table6_view, name='table2'),
+    path('table3/', table3_view, name='table3'),
+    path('table4/', table4_view, name='table4'),
+    path('table5/', table5_view, name='table5'),
+    path('table6/', table6_view, name='table6'),
     path('alltable/',all_tables_view, name='alltable'),
     
     
